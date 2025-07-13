931d71c50ba8ba1fcbf139cde767ea7cca0ca097
Jiankun Wei <72998341+david-wei-01001@users.noreply.github.com>
2025-07-12 18:14:26 -0600

    Update worker_base.py

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -146,6 +146,7 @@ class DelegateWorkerBase(WorkerBase):
     ) -> None:
         vllm_config: VllmConfig = kwargs.get("vllm_config")
         cls = resolve_obj_by_qualname(vllm_config.parallel_config.worker_cls)
+        logger.info(vllm_config.parallel_config.worker_cls)
         self.worker = cls(*args, **kwargs)
 
     def init_device(self) -> None:
